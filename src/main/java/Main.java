126e014394f5f2b79b08b2e6efef8c46a4ed2bf1
Hazem Essam <hazemkwita123@gmail.com>
2024-07-17 06:14:00 +0200

    Handle not found paths

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+  static final Number HTTP_PORT = 4221;
+  static final String HTTP_VERSION = "HTTP/1.1";
+
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
     try {
-      ServerSocket serverSocket = new ServerSocket(4221);
-
+      ServerSocket serverSocket = new ServerSocket((int) HTTP_PORT);
+      
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
+      
+      System.out.println(String.format("Server is running on port %d...", HTTP_PORT));
 
       Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
+      System.out.println("Accepted new connection");
 
+      String reqLine =  getReqLine(clientSocket);      
+      String reqPath = reqLine.split(" ")[1];
+      System.out.println(String.format("Requested path is: %s", reqPath));
+
+      Boolean isValidPath = reqPath.equals("/");
+      Number statusCode = isValidPath ? 200 : 404;
+      String statusText = isValidPath ? "OK" : "Not Found";
+      
       // Handle response
-      String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+      String httpResponse = String.format("%s %d %s\r\n\r\n", HTTP_VERSION, statusCode, statusText);
       clientSocket.getOutputStream().write(httpResponse.getBytes());
       
       // Close connection
@@ -27,5 +39,11 @@ public class Main {
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  public static String getReqLine(Socket clientSocket) throws IOException {
+    var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    String reqLine = reader.readLine();
+    return reqLine;
   }
 }
